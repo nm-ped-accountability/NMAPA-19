4cb388139f86611c1d25677794112f795e12f899
jeanho1 <35690580+jeanho1@users.noreply.github.com>
2019-08-21 16:30:34 -0600

    Add files via upload

--- NMAPA_Grade_Check.py
+++ NMAPA_Grade_Check.py
@@ -41,9 +41,14 @@ schools_web_df = pd.read_csv('Master Schools 2019 V3.csv', header=0,dtype={'schn
 # Choose SY = 2019
 schools_web_df = schools_web_df.loc[schools_web_df['SY'] == '2019']
 
-schools_web_df = schools_web_df[['schnumb','LoGrade','HiGrade','level']]
+schools_web_df = schools_web_df[['schnumb','LoGrade','HiGrade','level','schname','distname']]
 
 schools_web_df.rename(columns={'schnumb':'Code'}, inplace=True)
+schools_web_df.rename(columns={'schname':'v_schname'}, inplace=True)
+schools_web_df.rename(columns={'distname':'v_distname'}, inplace=True)
+
+
+
 
 df['Code'] = df['Code'].astype(float)
 dataframe = [df,schools_web_df]
@@ -57,7 +62,81 @@ merged_df['School_Grade_Range'] = merged_df['LoGrade'] + '-' + merged_df['HiGrad
 cross_tab_grade = pd.crosstab(merged_df.School_Grade_Range, merged_df.Test_Grade, margins=True)  # school code 126 missing from vendor
 cross_tab_gradeII = pd.crosstab(merged_df.level, merged_df.Test_Grade, margins=True)
 
-cross_tab_grade.to_csv("school_grade_grade_cross.csv", sep=',', encoding='utf-8-sig', index = True)
-cross_tab_gradeII.to_csv("school_grade_grade_crossII.csv", sep=',', encoding='utf-8-sig', index = True)
-print(cross_tab_grade)
-print(cross_tab_gradeII)
+#cross_tab_grade.to_csv("school_grade_grade_cross.csv", sep=',', encoding='utf-8-sig', index = True)
+#cross_tab_gradeII.to_csv("school_grade_grade_crossII.csv", sep=',', encoding='utf-8-sig', index = True)
+
+####### Merge MasterSchools with S_SCHNUMB #############################################################################
+merged_df['S_SCHNUMB'] = merged_df['S_DISTRICT_CODE'] * 1000 + merged_df['S_LOCATION_CODE']
+
+S_schools_web_df = schools_web_df.copy()
+
+S_schools_web_df.rename(columns={'Code':'S_SCHNUMB'}, inplace=True)
+S_schools_web_df.rename(columns={'LoGrade':'S_LoGrade'}, inplace=True)
+S_schools_web_df.rename(columns={'HiGrade':'S_HiGrade'}, inplace=True)
+S_schools_web_df.rename(columns={'level':'S_level'}, inplace=True)
+S_schools_web_df.rename(columns={'v_schname':'S_schname'}, inplace=True)
+S_schools_web_df.rename(columns={'v_distname':'S_distname'}, inplace=True)
+
+dataframe = [merged_df,S_schools_web_df]
+
+#merge dataframe to add school names
+S_merged_df = reduce(lambda left,right: pd.merge(left,right,on=['S_SCHNUMB'], how='outer'), dataframe)
+
+
+S_merged_df['S_School_Grade_Range'] = S_merged_df['S_LoGrade'] + '-' + S_merged_df['S_HiGrade'] + ' '+ 'Grade'
+
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-4 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'Code'] = S_merged_df['S_SCHNUMB']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-4 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'District'] = S_merged_df['S_schname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-4 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School'] = S_merged_df['S_distname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-4 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School_Grade_Range'] = S_merged_df['S_School_Grade_Range']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-4 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'DistrictCode'] = S_merged_df['S_DISTRICT_CODE']
+
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'Code'] = S_merged_df['S_SCHNUMB']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'District'] = S_merged_df['S_schname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School'] = S_merged_df['S_distname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School_Grade_Range'] = S_merged_df['S_School_Grade_Range']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '-1-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'DistrictCode'] = S_merged_df['S_DISTRICT_CODE']
+
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '0-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'Code'] = S_merged_df['S_SCHNUMB']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '0-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'District'] = S_merged_df['S_schname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '0-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School'] = S_merged_df['S_distname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '0-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School_Grade_Range'] = S_merged_df['S_School_Grade_Range']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '0-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'DistrictCode'] = S_merged_df['S_DISTRICT_CODE']
+
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '3-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'Code'] = S_merged_df['S_SCHNUMB']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '3-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'District'] = S_merged_df['S_schname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '3-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School'] = S_merged_df['S_distname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '3-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'School_Grade_Range'] = S_merged_df['S_School_Grade_Range']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '3-5 Grade') & (S_merged_df['Test_Grade'] == '6-8 Grade'), 'DistrictCode'] = S_merged_df['S_DISTRICT_CODE']
+
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '6-8 Grade') & (S_merged_df['Test_Grade'] == '10-11 Grade'), 'Code'] = S_merged_df['S_SCHNUMB']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '6-8 Grade') & (S_merged_df['Test_Grade'] == '10-11 Grade'), 'District'] = S_merged_df['S_schname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '6-8 Grade') & (S_merged_df['Test_Grade'] == '10-11 Grade'), 'School'] = S_merged_df['S_distname']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '6-8 Grade') & (S_merged_df['Test_Grade'] == '10-11 Grade'), 'School_Grade_Range'] = S_merged_df['S_School_Grade_Range']
+S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '6-8 Grade') & (S_merged_df['Test_Grade'] == '10-11 Grade'), 'DistrictCode'] = S_merged_df['S_School_Grade_Range']
+
+
+#S_merged_df.loc[(S_merged_df['School_Grade_Range'] == '0-8 Grade') & (S_merged_df['Test_Grade'] == '10-11 Grade'), 'School_Grade_Range'] = S_merged_df['S_School_Grade_Range']
+
+
+
+
+S_cross_tab_grade = pd.crosstab(S_merged_df.School_Grade_Range, S_merged_df.Test_Grade, margins=True)  # school code 126 missing from vendor
+S_cross_tab_gradeII = pd.crosstab(S_merged_df.S_level, S_merged_df.Test_Grade, margins=True)
+
+# select for a spcific value in a column in  a dataframe
+df_filter = (S_merged_df['School_Grade_Range'].isin(['-1-5 Grade']) == True) & (S_merged_df['Test_Grade'].isin(['6-8 Grade']))  # the ==False means not of isin 'variable'
+df_filter2 = (S_merged_df['School_Grade_Range'].isin(['0-8 Grade']) == True) & (S_merged_df['Test_Grade'].isin(['10-11 Grade']))  # the ==False means not of isin 'variable'
+
+# command returns the specific record.
+discrepant = (S_merged_df[df_filter2])
+#discrepant.to_csv("discrepant.csv", sep=',',encoding='utf-8', index = False )
+
+# 537984189 is a 3rd grade who took a 6th grade NMAPA test....
+#print(S_cross_tab_grade)
+#print(discrepant)
+
+#print(S_merged_df.head(5))
+#S_merged_df.to_csv("S_merged_df.csv", sep=',', encoding='utf-8-sig', index = True)
+
+
